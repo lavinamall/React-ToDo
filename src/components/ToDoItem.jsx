fda00716aafbc386815f021742bc3382f131a94d
Lavina Mall <lavina@apple.local>
2023-10-06 20:48:25 +0530

    add: css

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -57,28 +57,39 @@ function ToDoItem({ todo }) {
   }, []);
 
   return (
-    <div className={`${todo.completed ? "text-success" : "text-primary"} rounded my-3 p-2 border border-primary`}>
-      <input type="checkbox" checked={todo.completed} onChange={markAsComplete} />
+    <div className={`${todo.completed ? "text-success" : "text-primary"} rounded my-3 p-2 border border-primary bg-light-subtle`}>
+
+      <div className="d-flex align-items-center">
+        <input type="checkbox" checked={todo.completed} onChange={markAsComplete} />
+
+        <p className={`d-flex align-items-center mx-2 my-0 p-0 bg-transparent ${isTodoEditable ? "d-none" : "border border-0"} 
+        ${todo.completed ? "text-success font-italic text-decoration-line-through" : "text-primary"}`}>{todoMsg}</p>
+      </div>
+
       <input type="text"
-        className={`w-75 mx-2 bg-transparent ${isTodoEditable ? "border border-dark" : "border border-0"} ${todo.completed ? "text-success font-italic text-decoration-line-through" : "text-primary"}`}
+        className={`w-75 mx-2 bg-transparent ${isTodoEditable ? "border border-dark" : "d-none border border-0"} ${todo.completed ? "text-success font-italic text-decoration-line-through" : "text-primary"}`}
         value={todoMsg}
         onChange={(e) => setTodoMsg(e.target.value)}
         readOnly={!isTodoEditable}
       />
 
-      <div className="d-flex justify-content-between align-items-center">
+      {/* <div className="d-flex justify-content-between align-items-center"> */}
+      <div className={`d-flex justify-content-between align-items-center ${todo.completed ? "d-none" : ""}`}>
         <i className="text-muted mr-1" style={{ fontSize: '14px' }}>
           added  <span className="d-none d-sm-inline">on: {dateFormat(milisToDate(todo.id), "")} | </span>
           {formatElapsedTime(elapsedTime)} ago
         </i>
-        <div className="">
-          <button className="btn btn-sm btn-rounded btn-primary mx-1" type="button" onClick={handleEdit}>
+
+        <div>
+          <button className={`btn btn-sm btn-rounded btn-primary mx-1 ${todo.completed ? "d-none" : ""}`}
+            type="button" onClick={handleEdit}>
             {`${isTodoEditable ? "Save" : "Edit"}`}
           </button>
           <button className="btn btn-sm btn-rounded btn-danger mx-2" type="button" onClick={handleDelete}>
             Delete
           </button>
         </div>
+
       </div>
 
     </div>
